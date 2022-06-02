146d23340367cfff31705e6092b96b10a7ea654a
doanxuanduong <94898152+doanxuanduong@users.noreply.github.com>
2022-06-02 21:09:13 +0700

    code 02/06

--- lib/common/bloc/loading_bloc/loading_bloc.dart
+++ lib/common/bloc/loading_bloc/loading_bloc.dart
@@ -6,24 +6,24 @@ import 'loading_state.dart';
 class LoadingBloc extends Bloc<LoadingEvent, LoadingState> {
   factory LoadingBloc() => _instance;
 
-  LoadingBloc._() : super(Loaded());
+  LoadingBloc._() : super(Loaded()){
+    on<LoadingEvent>((event, emit){
+      switch (event.runtimeType) {
+        case StartLoading:
+          Loading();
+          break;
+        case FinishLoading:
+          Loaded();
+          break;
+        case ShowNewYearCardEvent:
+          ShowPopUpState(event.showNewYear);
+          break;
+      }
+    });
+  }
 
   static final LoadingBloc _instance = LoadingBloc._();
 
   static LoadingBloc get instance => _instance;
 
-  @override
-  Stream<LoadingState> mapEventToState(LoadingEvent event) async* {
-    switch (event.runtimeType) {
-      case StartLoading:
-        yield Loading();
-        break;
-      case FinishLoading:
-        yield Loaded();
-        break;
-      case ShowNewYearCardEvent:
-        yield ShowPopUpState(event.showNewYear);
-        break;
-    }
-  }
 }
