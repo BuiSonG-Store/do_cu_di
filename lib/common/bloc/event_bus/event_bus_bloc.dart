ea8caed1e8bc928545fe9e06dcdec2a8bbb79757
doanxuanduong <94898152+doanxuanduong@users.noreply.github.com>
2022-06-02 21:16:47 +0700

    code 02/06 2

--- lib/common/bloc/event_bus/event_bus_bloc.dart
+++ lib/common/bloc/event_bus/event_bus_bloc.dart
@@ -4,27 +4,26 @@ import 'event_bus_event.dart';
 import 'event_bus_state.dart';
 
 class EventBusBloc extends Bloc<EventBusEvent, EventBusState> {
-  EventBusBloc() : super(EventBusInitState());
-
-  @override
-  Stream<EventBusState> mapEventToState(EventBusEvent event) async* {
-    switch (event.runtimeType) {
-      case EventBusCheckCallEvent:
-        yield EventBusCheckCallState();
-        break;
-      case EventBusShowCallEvent:
-        yield EventBusShowCallState();
-        break;
-      case EventBusRequestInitDataCartEvent:
-        yield EventBusRequestInitDataCartState();
-        break;
-      case EventBusRequestInitDataNotificationEvent:
-        yield EventBusRequestInitDataNotificationState(
-            count: (event as EventBusRequestInitDataNotificationEvent).count);
-        break;
-      case EventBusRequestInitDataPersonalEvent:
-        yield EventBusRequestInitDataPersonalState();
-        break;
-    }
+  EventBusBloc() : super(EventBusInitState()){
+   on<EventBusEvent>((event, emit) {
+     switch (event.runtimeType) {
+       case EventBusCheckCallEvent:
+         EventBusCheckCallState();
+         break;
+       case EventBusShowCallEvent:
+         EventBusShowCallState();
+         break;
+       case EventBusRequestInitDataCartEvent:
+         EventBusRequestInitDataCartState();
+         break;
+       case EventBusRequestInitDataNotificationEvent:
+         EventBusRequestInitDataNotificationState(
+             count: (event as EventBusRequestInitDataNotificationEvent).count);
+     break;
+     case EventBusRequestInitDataPersonalEvent:
+       EventBusRequestInitDataPersonalState();
+     break;
+   }
+   });
   }
 }
--- lib/common/bloc/snackbar_bloc/snackbar_bloc.dart
+++ lib/common/bloc/snackbar_bloc/snackbar_bloc.dart
@@ -3,17 +3,16 @@ import 'package:do_cu_di/common/bloc/snackbar_bloc/snackbar_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SnackBarBloc extends Bloc<SnackbarEvent, SnackBarState> {
-  final duration = Duration(seconds: 3);
+  final duration = const Duration(seconds: 3);
 
-  SnackBarBloc() : super(InitialSnackbarState());
-
-  @override
-  Stream<SnackBarState> mapEventToState(SnackbarEvent event) async* {
-    if (event is ShowSnackbarEvent) {
-      yield ShowSnackBarState(
-          mess: event.content,
-          type: event.type,
-          duration: event.duration ?? duration);
-    }
+  SnackBarBloc() : super(InitialSnackbarState()){
+    on<SnackbarEvent>((event, emit) {
+      if (event is ShowSnackbarEvent) {
+        ShowSnackBarState(
+            mess: event.content,
+            type: event.type,
+            duration: event.duration ?? duration);
+      }
+    });
   }
 }
